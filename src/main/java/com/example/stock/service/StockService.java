2d7d9b4eba6a3c190b9f4226e45953991b56ca12
dkask <dkaskgkdua@gmail.com>
2022-09-03 15:32:46 +0900

    재고감소 기본로직

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,27 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockService {
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+
+    }
+
+}
